aa37becd8f0d68a11fc176f7b937da57f301f1a0
Emrah Isovic <emrahisovic20@gmail.com>
2024-10-04 22:00:10 +0200

    Dependency update

--- webpack.common.js
+++ webpack.common.js
@@ -15,4 +15,21 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require.resolve("sass"),
+            },
+          },
+        ],
+    },,
+    ],
+  },
 };
