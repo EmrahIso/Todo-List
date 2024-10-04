548748eaddf5b76f0a3cada04539358c48bbda5e
Emrah Isovic <emrahisovic20@gmail.com>
2024-10-04 21:38:17 +0200

    Added two new modules, for static listeners and storage

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,7 @@
+import "../css/style.css"; 
+import "./modules/listeners.js";
+
+// To make sure that we are in development mode
 if (process.env.NODE_ENV !== 'production') {
     console.log('Looks like we are in development mode!');
 }
---
+++ src/js/modules/listeners.js
@@ -0,0 +1,7 @@
+import { TaskManagerDOMControl } from "./DOMmanipulation.js";
+
+TaskManagerDOMControl.addProjectBtnEl.addEventListener("click", TaskManagerDOMControl.addProjectBtnClickEventHandler);
+
+TaskManagerDOMControl.addTaskBtnEl.addEventListener("click", TaskManagerDOMControl.addTaskBtnClickEventHandler);
+
+TaskManagerDOMControl.projectButtonsWrapperEl.addEventListener("click", TaskManagerDOMControl.projectButtonsClickEventHandler);
