850f9a24c86e467c190270eeb5e1314308ef7932
Emrah Isovic <emrahisovic20@gmail.com>
2024-10-13 23:04:07 +0200

    Added initial projects and tasks

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,63 @@
-import "../css/style.css"; 
+import "../scss/main.scss"; 
+import { addProjectToManager, switchActiveProject, addTaskToProject } from "./modules/logic.js";
+import { UIProjectCreator, UISwitchProjects, UITaskCreator } from "./modules/UI";
 import "./modules/listeners.js";
+
 
 // To make sure that we are in development mode
 if (process.env.NODE_ENV !== 'production') {
     console.log('Looks like we are in development mode!');
 }
+
+// Add Default Project 
+UIProjectCreator("Routine");
+addProjectToManager("Routine");
+
+switchActiveProject("Routine");
+UISwitchProjects();
+
+
+// Add Default Tasks to Routine Project
+
+const date = new Date();
+
+const day = date.getDate();
+const month = date.getMonth();
+const year = date.getFullYear();
+
+const brushYourTeethTaskData = {
+    title: "Brush Teeth",
+    description: "",
+    ["due date"]: `${year}-${month}-${day}`,
+    priority: "high",
+    notes: "",
+    checklist: false,
+}
+
+addTaskToProject("Routine", brushYourTeethTaskData);
+UITaskCreator("Routine", brushYourTeethTaskData);
+
+const goOutsideTaskData = {
+    title: "Go outside",
+    description: "Touch grass",
+    ["due date"]: `${year}-${month}-${day}`,
+    priority: "high",
+    notes: "Talk to someone, walk by the river",
+    checklist: true,
+}
+
+addTaskToProject("Routine", goOutsideTaskData);
+UITaskCreator("Routine", goOutsideTaskData);
+
+
+const workoutTaskData = {
+    title: "Workout",
+    description: "Chest, Shoulders, Triceps",
+    ["due date"]: `${year}-${month}-${day}`,
+    priority: "mid",
+    notes: "bench, dips, push ups, lateral raises",
+    checklist: false,
+}
+
+addTaskToProject("Routine", workoutTaskData);
+UITaskCreator("Routine", workoutTaskData);
